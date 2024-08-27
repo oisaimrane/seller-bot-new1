4f2ce934b67190c5bf2d972aa282e98b226cec34
oisaimrane <95139857+oisaimrane@users.noreply.github.com>
2024-08-27 12:04:25 +0100

    Update autorespond.js

--- source/events/autorespond.js
+++ source/events/autorespond.js
@@ -1,40 +1,33 @@
 const client = require('../../main.js');
-const config = require(`../../config.json`)
-const db = require('pro.db')
+const config = require('../../config.json');
+const db = require('pro.db');
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
-const {MessageEmbed , MessageButton , MessageActionRow} = require("discord.js")
-client.on("messageCreate", async message => { 
+client.on("messageCreate", async (message) => { 
+    // Ignore messages from bots and non-guild messages
+    if (message.author.bot || !message.guild) return;
 
-    // Normal Commands Handling
-    if (message.author.bot) return;
-    if (!message.guild) return;
-	if(message.content.startsWith("!buy")) {
-		const embed = new MessageEmbed()
-		.setDescription(`This Command Has Moved To Slash Commands.. Try **/buy** Instead`)
-		.setColor(message.guild.me.displayHexColor)
-		const button = new MessageButton()
-		.setLabel(`More Info`)
-		.setURL("https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ")
-		.setStyle("LINK")
-			const m = new MessageActionRow()
-		.addComponents(button)
-		message.reply({embeds:[embed] , components: [m]})
-		
-		
-	}
-	if(message.content.startsWith("!stock")) {
-		const embed = new MessageEmbed()
-		.setDescription(`This Command Has Moved To Slash Commands.. Try **/stock** Instead`)
-		.setColor(message.guild.me.displayHexColor)
-		const button = new MessageButton()
-		.setLabel(`More Info`)
-		.setURL("https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ")
-		.setStyle("LINK")
-			const m = new MessageActionRow()
-		.addComponents(button)
-		message.reply({embeds:[embed] , components: [m]})
-		
-		
-	}
+    // Define a function to create a response embed and button
+    const createResponse = (command) => {
+        const embed = new MessageEmbed()
+            .setDescription(`This Command Has Moved To Slash Commands.. Try **/${command}** Instead`)
+            .setColor(message.guild.me.displayHexColor);
 
-})
+        const button = new MessageButton()
+            .setLabel(`More Info`)
+            .setURL("https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ")
+            .setStyle("LINK");
+
+        const actionRow = new MessageActionRow().addComponents(button);
+        return { embeds: [embed], components: [actionRow] };
+    };
+
+    // Command handling with slash command notification
+    if (message.content.startsWith("!buy")) {
+        message.reply(createResponse('buy'));
+    }
+
+    if (message.content.startsWith("!stock")) {
+        message.reply(createResponse('stock'));
+    }
+});
